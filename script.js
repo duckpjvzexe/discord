efdf0a44f4605c12fa3927a5c80e71bdaa92cf61
duckpjvzexe <hoangconghung398@gmail.com>
2025-12-21 14:47:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -156,3 +156,19 @@ if (searchInput) {
     });
   });
 }
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 123) {
+        event.preventDefault();
+        return false;
+    }
+    if ((event.ctrlKey && event.shiftKey && (event.keyCode === 73 || event.keyCode === 74))) {
+        event.preventDefault();
+        return false;
+    }
+    if (event.ctrlKey && event.keyCode === 85) {
+        event.preventDefault();
+        return false;
+    }
+});
